c09d5d7bb0941e0c8f8ac2d45345f774f7ceef74
bitcon <143378003+vladislav957@users.noreply.github.com>
2024-09-13 19:12:44 +0300

    Add files via upload

---
+++ Bitcon v0.0.01/blockchain.db/blockchain.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE IF NOT EXISTS blockchain (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    index INTEGER,
+    timestamp TEXT,
+    data TEXT,
+    previous_hash TEXT,
+    hash TEXT,
+    nonce INTEGER
+);
