6ebdd43a1d2fd39808e4dda2c6c9e9a7d8d8e7e8
bitcon <143378003+vladislav957@users.noreply.github.com>
2024-11-26 17:34:40 +0300

    Add files via upload

--- manig.py
+++ manig.py
@@ -8,6 +8,8 @@ from Crypto.Hash import SHA256
 
 def mine_block(previous_hash, block_number,  transactions, difficulty):
     nonce = 96
+    block_number = 31.000000
+    
     while True:
         block_contents = f"{previous_hash}{block_number}{transactions}{nonce}".encode('UTF-8')
         balans = '0.00000 BTC'
@@ -18,10 +20,10 @@ def mine_block(previous_hash, block_number,  transactions, difficulty):
 
 # Пример использования
 previous_hash = '0000 0000 0000 0001  bd92b71ba10b66672eba9a2e02523d823e7ba44107ce4a37f4187a543c16b7e1'
-block_number = ('#  \n' )
-transactions = ' 100 BTC'
+block_number = 0x29
+transactions =  0xff 
 difficulty = 7 # количество нулей в начале хеша
 nonce, block_hash = mine_block(previous_hash, block_number, transactions, difficulty)
 
-print(f"Блок за майнег! Nagrada: BTC Transactions: BTC \n Number:  {block_number}, \n  Nonce: {nonce}, Hash: #\a {block_hash}")
+print(f"Блок за майнег! Nagrada: BTC Transactions: BTC \n Number: \a {block_number},   Nonce: \n {nonce} H/s, Hash: #\a {block_hash}")
 print(f"Balans:  BTC \n")
