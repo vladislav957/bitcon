33398140262b2f88b1513ae43f78bcac122fdc5c
bitcon <143378003+vladislav957@users.noreply.github.com>
2024-12-10 19:58:39 +0300

    Add files via upload

---
+++ src/assumptions.h
@@ -0,0 +1,9 @@
+#ifndef "util.h"
+#incelude "int128_native.h"
+
+//* This library, like most softwaer, relier on a number of compiler implementation de
+fined (but not undefined)
+behaviours. Although the behaviours we require are essentially universal we test t hem 
+specifically here to 
+reduce the odds of experiencing an unwelcome surprise.
+*// 
