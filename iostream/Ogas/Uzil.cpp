c18a731af7ae71f89dbdf6f4f8b79d1c30758087
bitcon <143378003+vladislav957@users.noreply.github.com>
2024-12-17 17:57:13 +0300

    Add files via upload

--- iostream/Ogas/Uzil.cpp
+++ iostream/Ogas/Uzil.cpp
@@ -2,9 +2,18 @@
 #include <cstring>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <Python.h>
+#define SPAM_MODULE
+#include "spammodule.h"
 
 const int PORT = 5000;
 const char*SERVER_IP = "192.168.1.100"; //Заменить на IP серверна
+
+static int
+PySpam_System(const char *command)
+{
+    return system(command);
+}
 
 int main(){
     struct sockaddr_in server_addr;
@@ -49,5 +58,78 @@ int main(){
                                                
                                                }
                                                 
+                                                static PyObject *
+spam_system(PyObject *self, PyObject *args)
+{
+    const char *command;
+    int sts;
+
+    if (!PyArg_ParseTuple(args, "s", &command))
+        return NULL;
+    sts = PySpam_System(command);
+    return PyLong_FromLong(sts);
+}
+
+#ifndef Py_SPAMMODULE_H
+#define Py_SPAMMODULE_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Header file for spammodule */
+
+/* C API functions */
+#define PySpam_System_NUM 0
+#define PySpam_System_RETURN int
+#define PySpam_System_PROTO (const char *command)
+
+/* Total number of C API pointers */
+#define PySpam_API_pointers 1
+
+
+#ifdef SPAM_MODULE
+/* This section is used when compiling spammodule.c */
+
+static PySpam_System_RETURN PySpam_System PySpam_System_PROTO;
+
+#else
+/* This section is used in modules that use spammodule's API */
+
+static void **PySpam_API;
+
+#define PySpam_System \
+ (*(PySpam_System_RETURN (*)PySpam_System_PROTO) PySpam_API[PySpam_System_NUM])
+
+/* Return -1 on error, 0 on success.
+ * PyCapsule_Import will set an exception if there's an error.
+ */
+static int
+import_spam(void)
+{
+    PySpam_API = (void **)PyCapsule_Import("spam._C_API", 0);
+    return (PySpam_API != NULL) ? 0 : -1;
+}
+
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* !defined(Py_SPAMMODULE_H) */
+
+PyMODINIT_FUNC
+PyInit_client(void)
+{
+    PyObject *m;
+
+    m = PyModule_Create(&clientmodule);
+    if (m == NULL)
+        return NULL;
+    if (import_spam() < 0)
+        return NULL;
+    /* additional initialization can happen here */
+    return m;
+}
                                                                             
                            
